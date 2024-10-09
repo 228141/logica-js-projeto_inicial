83cc0ec5133fd16dccdd59d9bac0efd116003dee
228141 <jailton.sp@hotmail.com>
2024-10-09 13:25:27 -0300

    Estudando While

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 alert ('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
+let numeroSecreto = 5;
 console.log (numeroSecreto);
-let chute = prompt ('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-//se chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+//enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+    //se chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 
-} else {
-    alert('Você errou :(')
 }
-
 
 // if significa 'se'
 // else significa 'senão'
+// != - Diferente
